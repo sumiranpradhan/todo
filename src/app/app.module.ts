b649cee4f3d358a476ab6bb93991a507a800a0aa
Sumiran <43204798+antihero008@users.noreply.github.com>
2020-09-29 20:12:39 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoHeaderComponent } from './todo/todo-header/todo-header.component';
+import { TodoInputComponent } from './todo/todo-input/todo-input.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    TodoHeaderComponent,
+    TodoInputComponent,
+    TodoListComponent,
+    TodoFooterComponent
   ],
   imports: [
     BrowserModule,
